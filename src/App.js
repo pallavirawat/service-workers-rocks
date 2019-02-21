94bb19fa21ad2e6eb0a26efdfdc56aa293bf3117
Pallavi <mun0108@users.noreply.github.com>
2019-02-21 21:14:34 +0530

    add logic if service worker support is present and render the main page accordingly

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import React, { Component } from 'react';
+import React  from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
+const App = () => {
+    const serviceWorkerSupported = () =>{
+        return (<div>
+            <div>Yay!!! Congratulations.</div>
+            <div>Service Worker Supported</div>
+            <div>Your current browser is just awesome as you are ;)</div>
+        </div>);
+    }
+
+    const serviceWorkerNotSupported = () =>{
+        return (<div>
+            <div>Uh-oh.</div>
+            <div>Service Workers are not at all supported</div>
+            <div>You might want to live in current age and get a better browser</div>
+            <a
+                className="App-link"
+                href="https://caniuse.com/#feat=serviceworkers"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                I Need Some Help!
+            </a>
+        </div>);
+    }
+
+    const serviceWorkerSupportText = window.navigator.serviceWorker? serviceWorkerSupported() : serviceWorkerNotSupported()
+
+      return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {serviceWorkerSupportText}
         </header>
       </div>
     );
-  }
 }
 
 export default App;
